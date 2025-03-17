b7198dd42cf2854a233540ce50488f0451cd1dc1
MicheleCasani <casanimichele@gmail.com>
2025-03-17 14:33:18 +0100

    [UPD] add variabili and if

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//definisco le variabili
+let userAge = prompt('inserisci la tua età')
+let userKm = prompt('inserisci il km desiderato')
+let price = userKm * 0.21
+
+
+//definisco la variabile del prezzo finale
+let finalPrice = price;
+
+//controllo il valore inserito dall'utente con quello di sistema
+if (userAge > 65 { finalPrice * 0.6 })
+if (usaAge < 18 {finalPrice * 0.8})
+
+    console.log('il prezzo è di')
+
