33e1532892afae62db38a764a83f8d56ad84c20d
MicheleCasani <casanimichele@gmail.com>
2025-03-17 14:41:41 +0100

    [FIX*

--- js/script.js
+++ js/script.js
@@ -6,10 +6,15 @@ let price = userKm * 0.21
 
 //definisco la variabile del prezzo finale
 let finalPrice = price;
+finalPrice= finalPrice.toFixed(2);
 
 //controllo il valore inserito dall'utente con quello di sistema
-if (userAge > 65 { finalPrice * 0.6 })
-if (usaAge < 18 {finalPrice * 0.8})
+if (userAge > 65){
+    finalPrice * 0.6
+}
+if (usarAge < 18){
+    finalPrice * 0.8
+}
 
-    console.log('il prezzo è di')
+    console.log("il prezzo finale è di " + finalPrice + "€")
 
